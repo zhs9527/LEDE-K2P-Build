045f9370f408ffdfc8d9c94acb32b3e14e65db50
zhs9527 <59416652+zhs9527@users.noreply.github.com>
2020-01-18 22:06:54 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -7,3 +7,4 @@
 #=================================================
 Modify default Size
 sed -i 's/0xf60000/0x1f60000/g' target/linux/ramips/dts/mt7621_phicomm_k2p.dts
+sed -i 's/15744k/32448k/g' target/linux/ramips/image/mt7621.mk
